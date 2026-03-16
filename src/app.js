675c0e7ec961df3a20dfac222b8ecc320653cbd7
hussainbinali-codes <125689469+hussainbinali-codes@users.noreply.github.com>
2026-03-16 21:24:52 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,19 +28,8 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['https://deals-hawaiian-remote-walked.trycloudflare.com', 'https://linear-arlington-timber-fact.trycloudflare.com'
-
-,
-"https://constructed-catalog-rare-spray.trycloudflare.com"
-,
-"https://kelkoo-differently-jobs-interior.trycloudflare.com",
- "http://localhost:5173",
- "https://anything-receives-shorts-twins.trycloudflare.com",
- "https://quotes-overseas-dictionary-ranger.trycloudflare.com",
- "https://console-cookie-lifetime-estates.trycloudflare.com",
- "https://compiler-garmin-leonard-bacterial.trycloudflare.com"
-  ],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }));
 app.use(express.json({ limit: "5mb" }));
 app.use(morgan('combined'));
